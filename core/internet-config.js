99fe27a782a4515d62b2d5809bd53ddb716dcc42
elbiojrodriguez <51291186+elbiojrodriguez@users.noreply.github.com>
2025-07-26 20:02:20 -0300

    Update internet-config.js

--- core/internet-config.js
+++ core/internet-config.js
@@ -1,12 +1,10 @@
-// 🔧 URL do servidor de sinalização FireWeb
 export const SIGNALING_SERVER_URL = 'https://fireweb-sfe0.onrender.com';
 
-// Configurações de ICE Servers (facilmente modificáveis)
 export const getIceServers = () => {
   return [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { 
+    {
       urls: 'turn:meet-jit-si-turnrelay.jitsi.net:443?transport=tcp',
       username: 'guest',
       credential: 'guest'
